5fe47f66770dbd141c19f205bcf6121af7f902bf
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-27 17:31:34 +0300

    cookie error fix

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res, expiresIn) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
@@ -6,13 +6,14 @@ const generateTokenAndSetCookie = (userId, res, expiresIn) => {
   });
 
   const maxAge =
-    expiresIn === '7d' ? 7 * 24 * 60 * 60 * 1000 : 1 * 60 * 60 * 1000;
+    expiresIn === "7d" ? 7 * 24 * 60 * 60 * 1000 : 1 * 60 * 60 * 1000;
 
-  res.cookie('jwt', token, {
+  res.cookie("jwt", token, {
     maxAge, // MS
     httpOnly: true, // Prevent XSS attacks (cross-site scripting attacks),
-    sameSite: 'lax', // CSRF attacks cross-site request forgery attacks
-    secure: false,
+    sameSite: "lax", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV === "production",
+    domain: process.env.COOKIE_DOMAIN,
   });
 };
 
